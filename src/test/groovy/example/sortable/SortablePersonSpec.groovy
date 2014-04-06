c49fa7d7f3b2be008382f3c1e6fdf419332ac55e
craigatk <atk.craig@gmail.com>
2014-04-05 23:06:13 -0500

    Shortened sortable test to use comparison instead of list sorting

--- src/test/groovy/example/sortable/SortablePersonSpec.groovy
+++ src/test/groovy/example/sortable/SortablePersonSpec.groovy
@@ -6,28 +6,21 @@ import spock.lang.Unroll
 class SortablePersonSpec extends Specification {
     @Unroll
     def 'should sort person in scenario "#scenario"'() {
-        given:
-        SortablePerson person1 = new SortablePerson(
-                firstName: firstName1,
-                lastName: lastName1,
-                age: age1
-        )
-
+        when:
         SortablePerson person2 = new SortablePerson(
                 firstName: firstName2,
                 lastName: lastName2,
                 age: age2
         )
 
-        List<SortablePerson> unsortedPeople = [person2, person1]
-        boolean mutateList = false
-
-        when:
-        List<SortablePerson> sortedPeople = unsortedPeople.sort(mutateList)
+        SortablePerson person1 = new SortablePerson(
+                firstName: firstName1,
+                lastName: lastName1,
+                age: age1
+        )
 
         then:
-        assert sortedPeople[0] == person1
-        assert sortedPeople[1] == person2
+        assert person1 < person2
 
         where:
         lastName1 | firstName1  | age1 | lastName2 | firstName2 | age2 || scenario
