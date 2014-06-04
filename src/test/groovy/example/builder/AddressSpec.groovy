1caab34ba941a6b931559f1c10269903f9afe93d
Craig Atkinson <atk.craig@gmail.com>
2014-06-04 18:01:09 -0500

    Upgrading to Groovy 2.3.2 to support @Builder with Gradle

--- src/test/groovy/example/builder/AddressSpec.groovy
+++ src/test/groovy/example/builder/AddressSpec.groovy
@@ -1,13 +1,16 @@
 package example.builder
 
-import spock.lang.Ignore
 import spock.lang.Specification
 
-@Ignore
 class AddressSpec extends Specification {
     def 'should build address'() {
         when:
-        Address address = new Address().street('123 Main St').city('Minneapolis').state('MN').zip('55403')
+        Address address = Address.builder()
+                .street('123 Main St')
+                .city('Minneapolis')
+                .state('MN')
+                .zip('55403')
+                .build()
 
         then:
         assert address.street == '123 Main St'
